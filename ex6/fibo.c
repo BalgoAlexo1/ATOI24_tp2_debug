8c96a3e4e85d2b179aa4d006d3c32c5512817421
AlexandreB <barei.alexandre13@gmail.com>
2024-03-08 11:08:04 +0000

    Exos

--- ex6/fibo.c
+++ ex6/fibo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 unsigned long long fibonacci(unsigned long long n) {
-    if (n < 0) {
+    if (n <= 1) {
         return n;
     } else {
         return fibonacci(n - 1) + fibonacci(n - 2);
@@ -9,7 +9,7 @@ unsigned long long fibonacci(unsigned long long n) {
 }
 
 int main() {
-    unsigned long long n = 50; // Valeur de n choisie arbitrairement
+    unsigned long long n = 20; // Valeur de n choisie arbitrairement
 
     printf("Calcul du nombre de Fibonacci pour n = %llu\n", n);
     printf("Le résultat est : %llu\n", fibonacci(n));
